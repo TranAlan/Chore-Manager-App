d410b1c92f63116f34472e8e410a4c9ff784a0e8
Alan Tran <alantran29@gmail.com>
2017-11-27 16:56:00 -0500

    linked newChOre to ChoreList

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ChoreListActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ChoreListActivity.java
@@ -1,7 +1,9 @@
 package com.example.alan.peter.bilal.sam.choremanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChoreListActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ChoreListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chore_list);
+    }
+
+    protected void createNewChoreButton(View view){
+        Intent intent = new Intent(this, NewChoreActivity.class);
+        startActivity(intent);
     }
 }
--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/NewChoreActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/NewChoreActivity.java
@@ -1,7 +1,9 @@
 package com.example.alan.peter.bilal.sam.choremanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewChoreActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class NewChoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_chore);
+    }
+
+    protected void saveExitOnClick(View view){
+        Intent intent = new Intent(this, ChoreListActivity.class);
+        startActivity(intent);
     }
 }
