9a5677a3eb3f16b06624e562cd244be5b29c1f69
Alan Tran <alantran29@gmail.com>
2017-11-27 16:09:00 -0500

    Menu buttons bring to respective activities

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/AppLoginActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/AppLoginActivity.java
@@ -3,6 +3,7 @@ package com.example.alan.peter.bilal.sam.choremanager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -187,6 +188,8 @@ public class AppLoginActivity extends AppCompatActivity implements LoaderCallbac
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
         }
     }
 
--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/MenuActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/MenuActivity.java
@@ -1,7 +1,8 @@
 package com.example.alan.peter.bilal.sam.choremanager;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,4 +11,36 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
     }
+
+    protected void userImageOnClick(View view){
+        Intent intent = new Intent(this, UserMenu.class);
+        startActivity(intent);
+    }
+    protected void settingImageOnClick(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    protected void materialsImageOnClick(View view){
+        Intent intent = new Intent(this, MaterialsActivity.class);
+        startActivity(intent);
+    }
+
+
+    protected void choreListImageOnClick(View view){
+        Intent intent = new Intent(this, ChoreListActivity.class);
+        startActivity(intent);
+    }
+
+    protected void groceriesImageOnClick(View view){
+        Intent intent = new Intent(this, Groceries.class);
+        startActivity(intent);
+    }
+
+    protected void shoppingImageOnClick(View view){
+        Intent intent = new Intent(this, ShoppingListActivity.class);
+        startActivity(intent);
+    }
+
+
 }
