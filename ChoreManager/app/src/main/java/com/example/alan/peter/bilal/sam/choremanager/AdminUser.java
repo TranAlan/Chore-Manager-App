45fcef06f09089343519e2dbc868d8d8fe2ef309
Alan Tran <alantran29@gmail.com>
2017-11-27 17:48:07 -0500

    Added User and AdminUser methods

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/AdminUser.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/AdminUser.java
@@ -1,9 +1,43 @@
 package com.example.alan.peter.bilal.sam.choremanager;
 
+import android.media.Image;
+
+import java.util.Date;
+
 /**
  * Created by Alan on 11/27/2017.
  */
 
 public class AdminUser extends User{
 
+    public AdminUser(String username, String password, int totalPoints, Image profilePic){
+        super(username, password, totalPoints, profilePic);
+    }
+
+    public Chore createChore(String name, String description, String note, int pointsWorth, Repeated isRepeated, Date deadline){
+        return new Chore(name, description, note, pointsWorth, isRepeated, deadline);
+    }
+
+    public void assignChore(User user, Chore chore){
+        user.addToAssignedChores(chore);
+        chore.setAssignedTo(user);
+        //Remember about ChoreManagerProfile
+    }
+
+    public void deAssignChore(Chore chore){
+        chore.getAssignedTo().removeFromAssignedChores(chore);
+        chore.setAssignedTo(null);
+        //add to unassigned list?
+    }
+
+    public void deleteChore(Chore chore){
+
+        if(chore.getAssignedTo()!= null){
+            deAssignChore(chore);
+        }
+        //remove from ChoreManagerProfile
+        
+
+    }
 }
+
--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/User.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/User.java
@@ -8,11 +8,11 @@ import android.media.Image;
  */
 
 public class User {
-    String username;
-    String password;
-    int totalPoints;
-    Image profilePic;
-    ArrayList<Chore> assignedChores;
+    private String username;
+    private String password;
+    private int totalPoints;
+    private Image profilePic;
+    private ArrayList<Chore> assignedChores;
 
     public User(){
         username = null;
@@ -28,6 +28,54 @@ public class User {
         this.profilePic = profilePic;
         assignedChores = new ArrayList<Chore>();
     }
+
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+    public int getTotalPoints(){
+        return totalPoints;
+    }
+
+    public Image getProfilePic(){
+        return profilePic;
+    }
+
+    public ArrayList<Chore> getAssignedChores(){
+        return assignedChores;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setTotalPoints(int totalPoints){
+        this.totalPoints = totalPoints;
+    }
+
+    public void setProfilePic(Image profilePic){
+        this.profilePic = profilePic;
+    }
+
+    public void setAssignedChores(ArrayList<Chore> assignedChores){
+        this.assignedChores = assignedChores;
+    }
+
+    public void addToAssignedChores(Chore chore){
+        assignedChores.add(chore);
+    }
+
+    public void removeFromAssignedChores(Chore chore){
+        assignedChores.remove(chore);
+    }
+
 
 
 }
