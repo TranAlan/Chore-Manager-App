fb2f1a5999f3d731b9066849fe590bdd217e2dd8
Peter-Lam <peter.quach.lam@gmail.com>
2017-11-29 21:25:48 -0500

    Changed new chore items to spinners and made it a scroll view

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/NewChoreActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/NewChoreActivity.java
@@ -7,14 +7,18 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 public class NewChoreActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     // creating spinner for choretpye and adapter
-    Spinner choreTypeSpinner;
-    ArrayAdapter adapter;
+    Spinner choreTypeSpinner,repeatableSpinner,repeatableTimesSpinner;
+    ArrayAdapter choreAdapter,repeatbleAdapter, assignToAdapter;
+    NumberPicker numberPicker = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,35 @@ public class NewChoreActivity extends AppCompatActivity implements AdapterView.O
         //allows textfields to move above keyboard
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
-        // create adapter from string array in string.xml file
-        adapter = ArrayAdapter.createFromResource(this,R.array.choreTypeSpinner_Options,android.R.layout.simple_spinner_item);
+        // create adapter from string array in string.xml file for ChoreTypeSpinner
+        choreAdapter = ArrayAdapter.createFromResource(this,R.array.choreTypeSpinner_Options,android.R.layout.simple_spinner_item);
         // set spinner to the one the the xml
         choreTypeSpinner =(Spinner) findViewById(R.id.choreTypeSpinner);
-        choreTypeSpinner.setAdapter(adapter);
+        choreTypeSpinner.setAdapter(choreAdapter);
         choreTypeSpinner.setOnItemSelectedListener(NewChoreActivity.this);
+
+        // create adapter from string array in string.xml file for RepeatableSpinner
+        repeatbleAdapter = ArrayAdapter.createFromResource(this,R.array.repeatableSpinner_Options,android.R.layout.simple_spinner_item);
+        // set spinner to the one the the xml
+        repeatableSpinner =(Spinner) findViewById(R.id.repeatableSpinner);
+        repeatableSpinner.setAdapter(repeatbleAdapter);
+        repeatableSpinner.setOnItemSelectedListener(NewChoreActivity.this);
+
+
+        // create adapter from string array in string.xml file for RepeatableSpinner
+        assignToAdapter = ArrayAdapter.createFromResource(this,R.array.userSpinner_Options,android.R.layout.simple_spinner_item);
+        // set spinner to the one the the xml
+        repeatableSpinner =(Spinner) findViewById(R.id.assignToSpiner);
+        repeatableSpinner.setAdapter(assignToAdapter);
+        repeatableSpinner.setOnItemSelectedListener(NewChoreActivity.this);
+
+        // Num Picker
+        numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+        numberPicker.setMaxValue(100);
+        numberPicker.setMinValue(1);
+        numberPicker.setWrapSelectorWheel(false);
+
+
     }
 
     @Override
