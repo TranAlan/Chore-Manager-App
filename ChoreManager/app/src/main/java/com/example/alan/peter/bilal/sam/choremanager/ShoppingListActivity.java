07afb435fdc1ba4302ee30058aef7df71784f0aa
Sam Rennie <samrennie77@gmail.com>
2017-11-30 23:00:04 -0500

    Added dialog for adding a shopping list item

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ShoppingListActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ShoppingListActivity.java
@@ -1,5 +1,8 @@
 package com.example.alan.peter.bilal.sam.choremanager;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +13,17 @@ public class ShoppingListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
     }
+
+
+    public Dialog onCreateItemPress() {
+        String[] choices = {"Material", "Grocery"};
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Create an item to add to the shopping list.").setItems(choices, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return alert.create();
+    }
 }
+
