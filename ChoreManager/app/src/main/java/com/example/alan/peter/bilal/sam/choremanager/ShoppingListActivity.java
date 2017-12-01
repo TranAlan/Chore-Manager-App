a411a56cba29634b404b8a4a64d448f2563223d0
Sam Rennie <samrennie77@gmail.com>
2017-12-01 16:08:58 -0500

    added dialogs for adding materials/groceries

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ShoppingListActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ShoppingListActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    //variables
+    private String toAdd = "";
+    private ChoreManagerProfile main = MenuActivity.getManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,32 +19,55 @@ public class ShoppingListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_list);
     }
 
-
-    public void onCreateItemPress(View view) {
-        String[] choices = {"Material","Grocery"};
+    //Method to add a new grocery item to the shopping list
+    public void onAddGroceryClick(View view) {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Create an item to add to the shopping list").setItems(choices, new DialogInterface.OnClickListener() {
-            @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
-                    }
-                });
+        alert.setMessage("Item to add:");
+        alert.setTitle("Add a grocery to Shopping List");
+        final EditText name = new EditText(this);
+        alert.setView(name);
 
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                //Your action here
+        //buttons
+        alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+                public void onClick(DialogInterface dialog, int which){
+                    toAdd = name.getText().toString();
+                    main.addShoppingGrocery(toAdd);
+                    //TODO: Find out how to update ChoreManagerProfile
+                }
+            });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.cancel();
             }
         });
-
-        alert.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener() {
-            @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                    }
-                });
-
         alert.show();
+        }
 
+    //Method to add a new Material  to the shopping list
+    public void onAddMaterialClock(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Item to add:");
+        alert.setTitle("Add a material to Shopping List");
+        final EditText name = new EditText(this);
+        alert.setView(name);
+
+        //buttons
+        alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                toAdd = name.getText().toString();
+                main.addShoppingMaterial(toAdd);
+                //TODO: Find out how to update ChoreManagerProfile
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.cancel();
+            }
+        });
+        alert.show();
     }
 }
-
