8a73eccff8f7d09c0b15d032c5c08d8181b66bcb
Peter-Lam <peter.quach.lam@gmail.com>
2017-12-01 21:56:57 -0500

    I forgot what I did

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ShoppingListActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ShoppingListActivity.java
@@ -4,7 +4,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
