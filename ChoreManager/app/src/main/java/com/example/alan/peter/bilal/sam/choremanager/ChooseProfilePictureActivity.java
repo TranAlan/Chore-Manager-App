7e709be591da941cce7507e908a7606f60a28619
Peter-Lam <peter.quach.lam@gmail.com>
2017-12-02 01:30:14 -0500

    Creating an activity to pick a profile picture

---
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/ChooseProfilePictureActivity.java
@@ -0,0 +1,28 @@
+package com.example.alan.peter.bilal.sam.choremanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ChooseProfilePictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_profile_picture);
+    }
+    public void SetTeamIcon(View view)
+    {
+        //Creating a Return intent to pass to the Main Activity
+        Intent returnIntent = new Intent();
+        //Figuring out which image was clicked
+        ImageView selectedImage = (ImageView) view;
+        //Adding stuff to the return intent
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+        //Finishing Activity and return to main screen!
+        finish();
+    }
+}
