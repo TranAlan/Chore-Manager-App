f4562a780c0cf4e554039204be4e2912cdd812d3
Peter-Lam <peter.quach.lam@gmail.com>
2017-12-01 23:47:28 -0500

    Added functionalty to Delete Selected and Add Item in Material List

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/MaterialsActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/MaterialsActivity.java
@@ -1,6 +1,5 @@
 package com.example.alan.peter.bilal.sam.choremanager;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -76,29 +75,34 @@ public class MaterialsActivity extends AppCompatActivity {
     }
 
     public void deleteSelectedOnClick(View view) {
-
-        //build an alert dialog
+        // Build an alert dialog
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setMessage("Are you sure you want to delete the selected items? This cannot be undone.");
-        alert.setTitle("Delete checked items");
-
-        //if user clicks cancel the dialogue closes without deleting checked items
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        // create a new view that inflates the custom shoppping list layout
+        View newView = getLayoutInflater().inflate(R.layout.delete_item_layout, null);
+        // declaring objects in the layout and linking them to the xml
+        final Button cancelButton = (Button) newView.findViewById(R.id.cancelButton);
+        final Button confirmButton = (Button) newView.findViewById(R.id.confirmButton);
+        alert.setView(newView);
+        //create Alert Dialog
+        final AlertDialog dialog = alert.create();
+        dialog.show();
+        // Listen and close the dialog if user clicks Cancel
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
+            public void onClick(View view) {
+                dialog.cancel();
             }
         });
-        //if user clicks confirm the selected items are deleted from material list
-        alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+        // Listen and close the dialog and deletes content if user clicks Confirm
+        confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            public void onClick(View view) {
                 //delete items from material list
                 MenuActivity.getManager().getMaterials().removeAll(selected);
                 //update view
                 //TODO
                 //close dialog
-                dialogInterface.cancel();
+                dialog.cancel();
             }
         });
     }
