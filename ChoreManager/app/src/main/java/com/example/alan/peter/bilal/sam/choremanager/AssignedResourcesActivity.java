8a1df9677baffaefa10da7fa68131b6b7164f59e
Sam Rennie <samrennie77@gmail.com>
2017-12-04 18:23:36 -0500

    created array holding different categories of required materials

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/AssignedResourcesActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/AssignedResourcesActivity.java
@@ -8,8 +8,17 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssignedResourcesActivity extends AppCompatActivity {
     private ImageButton filterButton;
+    List<String> allUsedMaterials = new ArrayList<>();
+    List<String> cookingMaterials = new ArrayList<>();
+    List<String> cleaningMaterials = new ArrayList<>();
+    List<String> miscMaterials = new ArrayList<>();
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -25,6 +34,71 @@ public class AssignedResourcesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assigned_resources);
+
+        //generate lists used in the filters
+
+        //Unassigned
+        for(int i=0;  i<MenuActivity.getManager().getUnassignedChores().size(); i++){
+            allUsedMaterials.addAll(MenuActivity.getManager().getUnassignedChores().get(i).getReqResources());
+            if(MenuActivity.getManager().getUnassignedChores().get(i).isCleaning()){
+                cleaningMaterials.addAll(MenuActivity.getManager().getUnassignedChores().get(i).getReqResources());
+            }
+            if(MenuActivity.getManager().getUnassignedChores().get(i).isMisc()){
+                miscMaterials.addAll(MenuActivity.getManager().getUnassignedChores().get(i).getReqResources());
+            }
+            if(MenuActivity.getManager().getUnassignedChores().get(i).isCooking()){
+                cookingMaterials.addAll(MenuActivity.getManager().getUnassignedChores().get(i).getReqResources());
+            }
+        }
+
+        //Finished
+        for(int i=0;  i<MenuActivity.getManager().getFinishedChores().size(); i++){
+            allUsedMaterials.addAll(MenuActivity.getManager().getFinishedChores().get(i).getReqResources());
+            if(MenuActivity.getManager().getFinishedChores().get(i).isCleaning()){
+                cleaningMaterials.addAll(MenuActivity.getManager().getFinishedChores().get(i).getReqResources());
+            }
+            if(MenuActivity.getManager().getFinishedChores().get(i).isMisc()){
+                miscMaterials.addAll(MenuActivity.getManager().getFinishedChores().get(i).getReqResources());
+            }
+            if(MenuActivity.getManager().getFinishedChores().get(i).isCooking()){
+                cookingMaterials.addAll(MenuActivity.getManager().getFinishedChores().get(i).getReqResources());
+            }
+        }
+
+        //Assigned to reg users
+        for(int i=0; i<MenuActivity.getManager().getRegUsers().size(); i++){
+            for(int j=0; j<MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().size(); j++){
+                allUsedMaterials.addAll(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).getReqResources());
+                if(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).isCleaning()){
+                    cleaningMaterials.addAll(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).getReqResources());
+                }
+                if(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).isCooking()){
+                    cookingMaterials.addAll(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).getReqResources());
+                }
+                if(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).isMisc()){
+                    miscMaterials.addAll(MenuActivity.getManager().getRegUsers().get(i).getAssignedChores().get(j).getReqResources());
+                }
+            }
+
+        }
+
+        //assigned to admin users
+        for(int i=0; i<MenuActivity.getManager().getAdminUsers().size(); i++){
+            for(int j=0; j<MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().size(); j++){
+                allUsedMaterials.addAll(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).getReqResources());
+                if(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).isCleaning()){
+                    cleaningMaterials.addAll(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).getReqResources());
+                }
+                if(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).isCooking()){
+                    cookingMaterials.addAll(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).getReqResources());
+                }
+                if(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).isMisc()){
+                    miscMaterials.addAll(MenuActivity.getManager().getAdminUsers().get(i).getAssignedChores().get(j).getReqResources());
+                }
+            }
+        }
+
+
         //  ---- List View Code ---
         ListView choreListView = (ListView) findViewById(R.id.allResourcesList);
         // link buttons to the ones in XML
