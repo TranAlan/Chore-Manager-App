5004a3cf641ce5c1f765e880c725dae3f865341b
Peter-Lam <peter.quach.lam@gmail.com>
2017-12-05 18:19:35 -0500

    Fixed Activty Layering issue

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/SettingsActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/SettingsActivity.java
@@ -77,7 +77,7 @@ public class SettingsActivity extends AppCompatActivity implements OnItemSelecte
 
                     //Clear Stack and Make MenuScreen and Go back to Chore List
                     Intent mainIntent = new Intent(SettingsActivity.this, AppLoginActivity.class);
-                    mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(mainIntent);
                     finish();
                 }
