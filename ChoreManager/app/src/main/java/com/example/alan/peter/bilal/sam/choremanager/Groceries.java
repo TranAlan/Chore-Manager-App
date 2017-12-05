872f57c7536fe874b4d8459e528e60a7b6fe9ea3
samrennie77 <31905308+samrennie77@users.noreply.github.com>
2017-12-05 14:02:29 -0500

    Partial implementation of Groceries Activity

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/Groceries.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/Groceries.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -14,7 +15,6 @@ public class Groceries extends AppCompatActivity {
 
 
     //variables
-    private ArrayList<String> selected = new ArrayList<>();
     private ArrayList<String> groceryList = new ArrayList<>();
     private ListView groceryListView;
 
@@ -26,18 +26,17 @@ public class Groceries extends AppCompatActivity {
         // get all the items from pantry
         groceryList.addAll(MenuActivity.getManager().getPantry());
         groceryListView = (ListView) findViewById(R.id.groceryListView);
-        ArrayList<StateVO> listVOs = new ArrayList<>();
+
+        ArrayList<String> listVOs = new ArrayList<>();
         for (int i = 0; i < groceryList.size(); i++)
         {
-            StateVO stateVO = new StateVO();
-            stateVO.setTitle(groceryList.get(i));
-            stateVO.setSelected(false);
-            listVOs.add(stateVO);
+            listVOs.add(groceryList.get(i));
         }
 
-        CustomMaterialListAdapter myAdapter = new CustomMaterialListAdapter(Groceries.this, 0, listVOs);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Groceries.this, android.R.layout.simple_list_item_1, listVOs);
         groceryListView.setAdapter(myAdapter);
     }
+
     //Method to add a new material item to the shopping list
     public void addGroceryOnClick(View view) {
         // Build an alert dialog
@@ -67,63 +66,19 @@ public class Groceries extends AppCompatActivity {
                 String itemNameString = itemName.getText().toString().trim();
                 MenuActivity.getManager().addPantryItem(itemNameString);
                 MenuActivity.getFbRef().child(MenuActivity.getEmail()).child("ChoreManager").setValue(MenuActivity.getManager());
-                groceryList.add(itemNameString);
                 groceryList.clear();
                 groceryList.addAll(MenuActivity.getManager().getPantry());
                 groceryListView = (ListView) findViewById(R.id.groceryListView);
-                ArrayList<StateVO> listVOs = new ArrayList<>();
+                ArrayList<String> listVOs = new ArrayList<>();
                 for (int i = 0; i < groceryList.size(); i++)
                 {
-                    StateVO stateVO = new StateVO();
-                    stateVO.setTitle(groceryList.get(i));
-                    stateVO.setSelected(false);
-                    listVOs.add(stateVO);
+                    listVOs.add(groceryList.get(i));
                 }
 
-                CustomMaterialListAdapter myAdapter = new CustomMaterialListAdapter(Groceries.this, 0, listVOs);
+                ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Groceries.this, android.R.layout.simple_list_item_1, listVOs);
                 groceryListView.setAdapter(myAdapter);
                 dialog.cancel();
             }
         });
     }
-
-    public void deleteSelectedOnClick(View view) {
-        // Build an alert dialog
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        // create a new view that inflates the custom shoppping list layout
-        View newView = getLayoutInflater().inflate(R.layout.delete_item_layout, null);
-        // declaring objects in the layout and linking them to the xml
-        final Button cancelButton = (Button) newView.findViewById(R.id.cancelButton);
-        final Button confirmButton = (Button) newView.findViewById(R.id.confirmButton);
-        alert.setView(newView);
-        //create Alert Dialog
-        final AlertDialog dialog = alert.create();
-        dialog.show();
-        // Listen and close the dialog if user clicks Cancel
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.cancel();
-            }
-        });
-        // Listen and close the dialog and deletes content if user clicks Confirm
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //delete items from material list
-                MenuActivity.getManager().getPantry().removeAll(selected);
-                //update view
-                //TODO
-                //close dialog
-                dialog.cancel();
-            }
-        });
-    }
-
-
-
-
-
-
-
-    }
+}
