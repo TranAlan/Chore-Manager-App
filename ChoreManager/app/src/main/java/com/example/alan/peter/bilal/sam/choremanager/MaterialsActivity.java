837483a049f379f239536fa9a7b8f4414fbfdcd8
samrennie77 <31905308+samrennie77@users.noreply.github.com>
2017-12-05 12:22:40 -0500

    finished Materials Activity

--- ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/MaterialsActivity.java
+++ ChoreManager/app/src/main/java/com/example/alan/peter/bilal/sam/choremanager/MaterialsActivity.java
@@ -50,7 +50,17 @@ public class MaterialsActivity extends AppCompatActivity {
                 builder.setNeutralButton("Delete Item", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        //IMPLEMENT
+                        MenuActivity.getManager().getMaterials().remove(item);
+                        materialList.clear();
+                        materialList.addAll(MenuActivity.getManager().getMaterials());
+                        //creates a list of the groceries on the shopping list
+                        ArrayList<String> listVOs = new ArrayList<>();
+                        for (int j = 0; j < materialList.size(); j++)
+                        {
+                            listVOs.add(materialList.get(j));
+                        }
+                        ArrayAdapter<String> myNewAdapter = new ArrayAdapter<String>(MaterialsActivity.this, android.R.layout.simple_list_item_1, listVOs);
+                        materialsListView.setAdapter(myNewAdapter);
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -64,17 +74,13 @@ public class MaterialsActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
-        ArrayList<StateVO> listVOs = new ArrayList<>();
+        ArrayList<String> listVOs = new ArrayList<>();
         for (int i = 0; i < materialList.size(); i++)
         {
-            StateVO stateVO = new StateVO();
-            stateVO.setTitle(materialList.get(i));
-            stateVO.setSelected(false);
-            listVOs.add(stateVO);
+            listVOs.add(materialList.get(i));
         }
-
-        CustomMaterialListAdapter myAdapter = new CustomMaterialListAdapter(MaterialsActivity.this, 0, listVOs);
-        materialsListView.setAdapter(myAdapter);
+        ArrayAdapter<String> myNewAdapter = new ArrayAdapter<String>(MaterialsActivity.this, android.R.layout.simple_list_item_1, listVOs);
+        materialsListView.setAdapter(myNewAdapter);
     }
 
     //Method to add a new material item to the shopping list
@@ -110,50 +116,13 @@ public class MaterialsActivity extends AppCompatActivity {
                 materialList.clear();
                 materialList.addAll(MenuActivity.getManager().getMaterials());
                 materialsListView = (ListView) findViewById(R.id.materialsListView);
-                ArrayList<StateVO> listVOs = new ArrayList<>();
+                ArrayList<String> listVOs = new ArrayList<>();
                 for (int i = 0; i < materialList.size(); i++)
                 {
-                    StateVO stateVO = new StateVO();
-                    stateVO.setTitle(materialList.get(i));
-                    stateVO.setSelected(false);
-                    listVOs.add(stateVO);
+                    listVOs.add(materialList.get(i));
                 }
-
-                CustomMaterialListAdapter myAdapter = new CustomMaterialListAdapter(MaterialsActivity.this, 0, listVOs);
-                materialsListView.setAdapter(myAdapter);
-                dialog.cancel();
-            }
-        });
-    }
-
-    public void deleteSelectedOnClick(View view) {
-        // Build an alert dialog
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        // create a new view that inflates the custom shoppping list layout
-        View newView = getLayoutInflater().inflate(R.layout.delete_item_layout, null);
-        // declaring objects in the layout and linking them to the xml
-        final Button cancelButton = (Button) newView.findViewById(R.id.cancelButton);
-        final Button confirmButton = (Button) newView.findViewById(R.id.confirmButton);
-        alert.setView(newView);
-        //create Alert Dialog
-        final AlertDialog dialog = alert.create();
-        dialog.show();
-        // Listen and close the dialog if user clicks Cancel
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.cancel();
-            }
-        });
-        // Listen and close the dialog and deletes content if user clicks Confirm
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //delete items from material list
-                MenuActivity.getManager().getMaterials().removeAll(selected);
-                //update view
-                //TODO
-                //close dialog
+                ArrayAdapter<String> myNewAdapter = new ArrayAdapter<String>(MaterialsActivity.this, android.R.layout.simple_list_item_1, listVOs);
+                materialsListView.setAdapter(myNewAdapter);
                 dialog.cancel();
             }
         });
